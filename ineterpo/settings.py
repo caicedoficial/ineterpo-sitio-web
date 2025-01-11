0bd7873753114daa69f911b100563f31704475c0
Alexander <caicedoficial@gmail.com>
2025-01-11 12:20:29 -0500

    Agregar la funcionalidad de gestión de eventos con modelos, vistas y administración en Django

--- ineterpo/settings.py
+++ ineterpo/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "noticias.apps.NoticiasConfig",
+    "eventos.apps.EventosConfig",
     "compressor",
     "django_browser_reload",
 ]
