9739514877b8a2da315a2fd2ec1c45ece217cc41
Alexander <caicedoficial@gmail.com>
2025-01-15 11:28:01 -0500

    Actualizar la lógica de consulta de noticias, modificar el campo de fecha a DateTimeField y mejorar estilos en las secciones de noticias.

--- noticias/views.py
+++ noticias/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Noticias
 
 def noticias(request):
-    noticias = Noticias.objects.all().order_by('-fecha')[:7]
+    noticias = Noticias.objects.all().order_by('-fecha')
 
     return render(request, 'noticias.html', {'noticias': noticias})
