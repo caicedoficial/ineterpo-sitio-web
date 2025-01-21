402838ca883c36b71b31e2197ff56e01f556fff5
Alexander <caicedoficial@gmail.com>
2025-01-21 13:53:11 -0500

    Actualizar configuración de compresión: habilitar compresión offline y ajustar carga de scripts en la plantilla principal.

--- ineterpo/settings.py
+++ ineterpo/settings.py
@@ -112,7 +112,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Compressor settings
 COMPRESS_ROOT = BASE_DIR / "static"
 COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
